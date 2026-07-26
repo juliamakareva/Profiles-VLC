78c67ba736a0cb3ef1787f929e25a9e0536811e9
juliamakareva <julia.makareva@gmail.com>
2026-07-26 14:00:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -251,4 +251,11 @@ def api_stats():
         "classifications": dict(classif_counter),
     })
 
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="127.0.0.1",
+        port=5000
+    )
+
 
